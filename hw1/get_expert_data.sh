72d2c7518c7f39dd84aac32934ef2ccbea7e33b1
Joao Carreira <joao@berkeley.edu>
2017-09-10 18:46:25 -0700

    Code and data observation/action of experts

---
+++ hw1/get_expert_data.sh
@@ -0,0 +1,7 @@
+python3 run_expert.py experts/Humanoid-v1.pkl Humanoid-v1
+python3 run_expert.py experts/Ant-v1.pkl Ant-v1
+python3 run_expert.py experts/HalfCheetah-v1.pkl HalfCheetah-v1
+python3 run_expert.py experts/Hopper-v1.pkl Hopper-v1
+python3 run_expert.py experts/Reacher-v1.pkl Reacher-v1
+python3 run_expert.py experts/Walker2d-v1.pkl Walker2d-v1
+
